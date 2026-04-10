5939609f1c50808395c7c086d0d7ea4621e4e0ce
shqipezeneli <shqipe.zeneli3@student.uni-pr.edu>
2026-04-11 00:35:26 +0200

    feat: config setup

---
+++ server/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'server_ip' => '0.0.0.0',
+    'udp_port' => 9000,
+    'http_port' => 8080,
+    'max_clients' => 5,
+    'timeout' => 30,
+    'admin_ip' => '127.0.0.1',
+    'log_file' => __DIR__ . '/.../logs.txt',
+    'upload_file' => __DIR__ . '/shared/files/'
+];
