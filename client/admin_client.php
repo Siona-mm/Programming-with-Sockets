160f5efac7b560b8dd89defe41dab6ba687be0fd
Sara Vishi <vishisara@gmail.com>
2026-04-19 22:27:11 +0200

    Client implementation

--- client/admin_client.php
+++ client/admin_client.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../server/config.php';
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 
 echo "--- UDP ADMIN CLIENT ---\n";
-echo "Komandat: /list, /read <file>, /upload <file>\n";
+echo "Komandat: /list, /read <file>, /upload <file>, /download <file>, /delete <file>, /search <keyword>, /info <file>\n";
 
 while (true) {
     $msg = readline("Shkruaj komanden: ");
@@ -12,7 +12,7 @@ while (true) {
 
     socket_sendto($socket, $msg, strlen($msg), 0, SERVER_IP, UDP_PORT);
 
-    socket_recvfrom($socket, $reply, 2048, 0, $from_ip, $from_port);
+    socket_recvfrom($socket, $reply, 4096, 0, $from_ip, $from_port);
     echo "Serveri ktheu: " . $reply . "\n\n";
 }
 
